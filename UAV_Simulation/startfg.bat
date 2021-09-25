cf1a2d106686a9f99fb5b68637580e9feb24acbb
root <root@DESKTOP-TTIJS3R.localdomain>
2021-09-25 19:41:04 -0400

    uav simulation

---
+++ UAV_Simulation/startfg.bat
@@ -0,0 +1,24 @@
+"C:\Program Files\FlightGear 3.4.0\bin\fgfs" ^
+    "--fg-root=C:\Program Files\FlightGear 3.4.0\data" ^
+    "--aircraft=Rascal110-JSBSim" ^
+    "--geometry=400x300" ^
+    "--native-fdm=socket,in,10,192.168.56.1,5503,udp" ^
+    "--fdm=external" ^
+    "--vc=30" ^
+    "--altitude=10000" ^
+    "--heading=90" ^
+    "--roll=0" ^
+    "--pitch=0" ^
+    "--wind=0@0" ^
+    "--turbulence=0.0" ^
+    "--prop:/sim/frame-rate-throttle-hz=30" ^
+    "--timeofday=noon" ^
+    "--shading-flat" ^
+    "--fog-disable" ^
+    "--disable-specular-highlight" ^
+    "--disable-random-objects" ^
+    "--disable-panel" ^
+    "--disable-horizon-effect" ^
+    "--disable-clouds" ^ 
+    "--disable-anti-alias-hud"
+	
